bf93b30d86cf9471f9d4ab6c4de2e5ede48dbb46
Yansen <hanssn88@gmail.com>
2022-11-13 09:18:28 +0700

    first ...

--- server.js
+++ server.js
@@ -4,27 +4,27 @@ const db = require("./app/models");
 const app = express();
 
 const corsOptions = {
-  origin: "*",
+    origin: "*",
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
 const mongoCoonfig = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 };
 db.mongoose.connect(db.url, mongoCoonfig)
-    .then(()=>console.log("Database connected ...."))
-    .catch(err=>{
-      console.log(`Gagal konek DB ${err.message}`);
-      process.exit();
+    .then(() => console.log("Database connected ...."))
+    .catch(err => {
+        console.log(`Gagal konek DB ${err.message}`);
+        process.exit();
     })
 
 require("./app/routes/mahaiswa.route")(app);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello ...." });
+    res.json({message: "Hello ...."});
 });
 
 const PORT = process.env.PORT || 8000;
